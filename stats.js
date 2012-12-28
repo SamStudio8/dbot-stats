4a661e0b6f0fd92ab953e0251b6c685fafd6f2a1
Sam Nicholls <sam.n@studio8media.co.uk>
2012-12-28 15:27:25 +0000

    Remove unnecessary regex, add ~lincent default action

--- stats.js
+++ stats.js
@@ -9,7 +9,7 @@ var stats = function(dbot){
                     event.reply(dbot.t("user_lines", {
                         "user": input,
                         "chan": event.channel,
-                        "lines": dbot.db.userStats[input][event.channel]["total_lines"],
+                        "lines": dbot.db.userStats[input][event.channel]["total_lines"].numberFormat(0),
                         "start": new Date(dbot.db.userStats[input][event.channel]["startstamp"]).toDateString()}
                     ));
                 }
@@ -22,7 +22,7 @@ var stats = function(dbot){
             else{
                 event.reply(dbot.t("chan_lines", {
                     "chan": event.channel,
-                    "lines": dbot.db.chanStats[event.channel]["total_lines"],
+                    "lines": dbot.db.chanStats[event.channel]["total_lines"].numberFormat(0),
                     "start": new Date(dbot.db.chanStats[event.channel]["startstamp"]).toDateString()}
                 ));
             }
@@ -46,6 +46,15 @@ var stats = function(dbot){
                         "user": input}
                     ));
                 }
+            }
+            else{
+                var percent = ((dbot.db.chanStats[event.channel]["users"][event.user]["lines"]
+                    / dbot.db.chanStats[event.channel]["total_lines"])*100);
+                event.reply(dbot.t("lines_percent", {
+                    "user": event.user,
+                    "chan": event.channel,
+                    "percent": percent.numberFormat(2),
+                    "lines": dbot.db.chanStats[event.channel]["users"][event.user]["lines"].numberFormat(0) }
             }
         },
 
@@ -74,8 +83,8 @@ var stats = function(dbot){
             var max_user = "nobody";
             for(var user in dbot.db.chanStats[event.channel]["users"]){
                 if(dbot.db.chanStats[event.channel]["users"].hasOwnProperty(user)){
-                    if(dbot.db.chanStats[event.channel]["users"][user] > max){
-                        max = dbot.db.chanStats[event.channel]["users"][user];
+                    if(dbot.db.chanStats[event.channel]["users"][user]["lines"] > max){
+                        max = dbot.db.chanStats[event.channel]["users"][user]["lines"];
                         max_user = user;
                     }
                 }
@@ -85,9 +94,6 @@ var stats = function(dbot){
             ));
         },
     };
-
-    commands['~lines'].regex = [/^~lines (\w{1,})?/, 2];
-    commands['~lincent'].regex = [/^~lincent (\w{1,})/, 2]
 
     return {
         'name': 'stats',
