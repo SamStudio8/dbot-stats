56d822fa129e04fd4529443b1fe5e8a7f9941c55
Sam Nicholls <sam.n@studio8media.co.uk>
2012-12-29 12:25:35 +0000

    Remove case sensitivity, store all nicks in lowercase [Close #7]

--- stats.js
+++ stats.js
@@ -6,7 +6,7 @@ var stats = function(dbot){
     var commands = {
         '~lines': function(event){
             if(event.params[1]){
-                var input = event.params[1].trim()
+                var input = event.params[1].trim().toLowerCase();
                 if(userStats[event.server].hasOwnProperty(input)){
                     event.reply(dbot.t("user_lines", {
                         "user": input,
@@ -32,7 +32,7 @@ var stats = function(dbot){
 
         '~words': function(event){
             if(event.params[1]){
-                var input = event.params[1].trim()
+                var input = event.params[1].trim().toLowerCase();
                 if(userStats[event.server].hasOwnProperty(input)){
                     event.reply(dbot.t("user_words", {
                         "user": input,
@@ -62,7 +62,7 @@ var stats = function(dbot){
 
         '~lincent': function(event){
             if(event.params[1]){
-                var input = event.params[1].trim();
+                var input = event.params[1].trim().toLowerCase();
                 if(chanStats[event.server][event.channel]["users"].hasOwnProperty(input)){
                     var percent = ((chanStats[event.server][event.channel]["users"][input]["lines"]
                         / chanStats[event.server][event.channel]["total_lines"])*100);
@@ -80,20 +80,21 @@ var stats = function(dbot){
                 }
             }
             else{
-                var percent = ((chanStats[event.server][event.channel]["users"][event.user]["lines"]
+                var user = event.user.trim().toLowerCase();
+                var percent = ((chanStats[event.server][event.channel]["users"][user]["lines"]
                     / chanStats[event.server][event.channel]["total_lines"])*100);
                 event.reply(dbot.t("lines_percent", {
                     "user": event.user,
                     "chan": event.channel,
                     "percent": percent.numberFormat(2),
-                    "lines": chanStats[event.server][event.channel]["users"][event.user]["lines"].numberFormat(0) }
+                    "lines": chanStats[event.server][event.channel]["users"][user]["lines"].numberFormat(0) }
                 ));
             }
         },
 
         '~active': function(event){
             if(event.params[1]){
-                var input = event.params[1].trim();
+                var input = event.params[1].trim().toLowerCase();
                 if(userStats[event.server].hasOwnProperty(input)){
                     var max = -1;
                     var max_index = -1;
@@ -179,12 +180,13 @@ var stats = function(dbot){
                 chanStats[event.server] = {}
             }
 
+            var user = event.user.trim().toLowerCase();
             // User-centric Stats
-            if(!userStats[event.server].hasOwnProperty(event.user)){
-                userStats[event.server][event.user] = {}
+            if(!userStats[event.server].hasOwnProperty(user)){
+                userStats[event.server][user] = {}
             }
-            if(!userStats[event.server][event.user].hasOwnProperty(event.channel)){
-                userStats[event.server][event.user][event.channel] = {
+            if(!userStats[event.server][user].hasOwnProperty(event.channel)){
+                userStats[event.server][user][event.channel] = {
                     "total_lines": 0,
                     "total_words": 0,
                     "freq_hours": {},
@@ -195,12 +197,12 @@ var stats = function(dbot){
                 
                 // Initialize hour frequency counters
                 for(var i=0; i<=23; i++){
-                    userStats[event.server][event.user][event.channel]["freq_hours"][i] = 0;
+                    userStats[event.server][user][event.channel]["freq_hours"][i] = 0;
                 }
             }
-            userStats[event.server][event.user][event.channel]["freq_hours"][event.time.getHours()] += 1;
-            userStats[event.server][event.user][event.channel]["total_lines"] += 1;
-            userStats[event.server][event.user][event.channel]["total_words"] += event.message.split(" ").length;
+            userStats[event.server][user][event.channel]["freq_hours"][event.time.getHours()] += 1;
+            userStats[event.server][user][event.channel]["total_lines"] += 1;
+            userStats[event.server][user][event.channel]["total_words"] += event.message.split(" ").length;
             
             // Channel-centric Stats
             if(!chanStats[event.server].hasOwnProperty(event.channel)){
@@ -217,12 +219,12 @@ var stats = function(dbot){
                     chanStats[event.server][event.channel]["freq_hours"][i] = 0;
                 }
             }
-            if(!chanStats[event.server][event.channel]["users"].hasOwnProperty(event.user)){
-                chanStats[event.server][event.channel]["users"][event.user] = {
+            if(!chanStats[event.server][event.channel]["users"].hasOwnProperty(user)){
+                chanStats[event.server][event.channel]["users"][user] = {
                     "lines": 0,
                     "mentions": 0 };
             }
-            chanStats[event.server][event.channel]["users"][event.user]["lines"] += 1;
+            chanStats[event.server][event.channel]["users"][user]["lines"] += 1;
             chanStats[event.server][event.channel]["freq_hours"][event.time.getHours()] += 1;
             chanStats[event.server][event.channel]["total_lines"] += 1;
             chanStats[event.server][event.channel]["total_words"] += event.message.split(" ").length;
@@ -231,13 +233,13 @@ var stats = function(dbot){
             if(dbot.db.hasOwnProperty("knownUsers")){
                 // Server key should exist in knownUsers
                 for (var i = 0; i < dbot.db.knownUsers[event.server].users.length; i++){
-                    var name = dbot.db.knownUsers[event.server].users[i];
+                    var name = dbot.db.knownUsers[event.server].users[i].trim().toLowerCase();
                     var toMatch = "\\b"+name+":?\\b"
                     if(event.message.search(toMatch) > -1){
-                        if(!userStats[event.server][event.user][event.channel]["out_mentions"].hasOwnProperty(name)){
-                            userStats[event.server][event.user][event.channel]["out_mentions"][name] = 0;
+                        if(!userStats[event.server][user][event.channel]["out_mentions"].hasOwnProperty(name)){
+                            userStats[event.server][user][event.channel]["out_mentions"][name] = 0;
                         }
-                        userStats[event.server][event.user][event.channel]["out_mentions"][name] += 1;
+                        userStats[event.server][user][event.channel]["out_mentions"][name] += 1;
                         userStats[event.server][name][event.channel]["in_mentions"] += 1;
                     }
                 }
